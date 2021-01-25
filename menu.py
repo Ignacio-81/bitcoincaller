723ae8c45baf49ffd64b0b18e048fd6710bf546e
Ignacio-81 <ignacio.badella@gmail.com>
2021-01-25 16:07:56 -0300

    Errors handling

--- menu.py
+++ menu.py
@@ -12,83 +12,103 @@ def clear():
     else:
         os.system('clear')
 
+def show_bc_screen (stat, data, **args):
+    if stat: 
+        if data == 'IFTTT': 
+            print('IFTTT Request Sended OK...')    
+        else: 
+            #print('Answer OK from server') 
+            print("Bitcoin value is: {0:,.2f} ".format(data['price']) + args['cur'])
+            print("Last 24hs variation is: {0:.2%}".format(data['percent_change_24h']/100))
+            print("Last 7days variation is: {0:.2%}".format(data['percent_change_7d']/100))
+    else: 
+        if data == 'IFTTT':
+            print('Error while sending IFTTT request: {}'.format(data))
+        else:    
+            print('Error while requesting info to BC URL:', data)
+
 def loop():
-
+    
+    bot = resident.watchbot(_running = False)
     while True:
+        try: 
+            clear()
 
-        clear()
+            print("========================")
+            print("    Bitcoin Manager     ")
+            print("========================")
+            print("[1] Ask bitcoin info USD")
+            print("[2] Ask bitcoin info ARS")
+            print("[3] Send BT to Mobile   ")
+            print("[4] Notification process")
+            print("[5] Empty               ")
+            print("[6] Exit                ")
+            print("========================")
 
-        print("========================")
-        print("    Bitcoin Manager     ")
-        print("========================")
-        print("[1] Ask bitcoin info USD")
-        print("[2] Ask bitcoin info ARS")
-        print("[3] Send BT to Mobile   ")
-        print("[4] Notification process")
-        print("[5] Empty               ")
-        print("[6] Exit                ")
-        print("========================")
+            option = input("> ")
 
-        option = input("> ")
+            clear()
 
-        clear()
-
-        if option == '1':
-            print("Retrieving Bitcoin USD Information...\n")
-            stat, data = comm.get_btdata('USD')
-            if stat: 
-                #print('Answer OK from server') 
-                print("Bitcoin value is: {0:,.2f} ".format(data['price']) + 'USD')
-                print("Last 24hs variation is: {0:.2%}".format(data['percent_change_24h']/100))
-                print("Last 7days variation is: {0:.2%}".format(data['percent_change_7d']/100))
-            else: 
-                print('Error while requesting info: {}'.format(data))
-        
-        if option == '2':
-            print("RetrievingBitcoin ARS Information...\n")
-            stat, data = comm.get_btdata('ARS')
-            if stat: 
-                print("Bitcoin value is: {0:,.2f} ".format(data['price']) + 'ARS')
-                print("Last 24hs variation is: {0:.2%}".format(data['percent_change_24h']/100))
-                print("Last 7days variation is: {0:.2%}".format(data['percent_change_7d']/100))
-            else: 
-                print('Error while requesting info: {}'.format(data))
-        
-        if option == '3':
-            print("Select the currency -> 1.USD / 2.ARS")
-            opt2 = input("> ")
-            print("Sending BC information to Mobile...\n")
-            if opt2 == '1':
+            if option == '1':
+                print("Retrieving Bitcoin USD Information...\n")
                 stat, data = comm.get_btdata('USD')
-                comm.post_IFTTT_event('bitcoin_USD_price', data)
-            if opt2 == '2':
+                show_bc_screen(stat,data, cur='USD')
+            
+            if option == '2':
+                print("RetrievingBitcoin ARS Information...\n")
                 stat, data = comm.get_btdata('ARS')
-                comm.post_IFTTT_event('bitcoin_ARS_price', data)    
+                show_bc_screen(stat, data, cur='ARS')
             
-            
-        if option == '4':
-            print("Please select to 1-RUN or 2-STOP background notification process:")
-            opt2 = input("> ")
-            if opt2 == '1':
-                print("Please enter notification time in HS...\n")
-                time = (float(input("> ")) * 3600)
-                print("Please enter BC threshold '%' change in the last hour ...\n")
-                p_val = input("> ")
-                print("Please enter BC minimum Threshold Value in USD...\n")
-                mval = input("> ")
-                print("Running notification process...\n")
+            if option == '3':
+                print("Select the currency -> 1.USD / 2.ARS")
+                opt2 = input("> ")
+                print("Sending BC information to Mobile...\n")
+                if opt2 == '1':
+                    cur = 'USD'
+                    
+                if opt2 == '2':
+                    cur = 'ARS'    
                 
-                bot = resident.watchbot(time,p_val,mval)
-            
-            if opt2 == '2':
-                print("Stopping notification process...\n")
-                bot.terminate()
+                stat, data = comm.get_btdata(cur)
+                if stat :
+                    stat, data = comm.post_IFTTT_event('bitcoin_'+cur+'_price', data)
+                show_bc_screen (stat, data)
 
-        if option == '5':
-            print("Empty...\n")
-            
-        if option == '6':
-            print("Closing app, Bye!...\n")
-            break
+            if option == '4':
+                print("Please select to 1-RUN or 2-STOP background notification process:")
+                opt2 = input("> ")
+                if opt2 == '1':
+                        if not bot._running : 
+                            print("\nPlease enter notification time in HS:")
+                            time = (float(input("> ")) * 3600)
+                            print("\nPlease enter BC threshold '%' change in the last hour ...")
+                            p_val = input("> ")
+                            print("\nPlease enter BC minimum Threshold Value in USD...")
+                            mval = input("> ")
+                            print("Running notification process...\n")  
+                            bot = resident.watchbot(True, time, p_val, mval)
+                        else:
+                            print("The Notification process is already running, please stop it first...") 
 
-        input("\nPress ENTER to continue...")
+                if opt2 == '2':
+                    if bot._running  : 
+                        print("Stopping notification process...\n")
+                        bot.terminate()
+                    else:
+                        print("Notification process was not running...\n")
+
+
+            if option == '5':
+                print("Empty...\n")
+                
+            if option == '6':
+                print("Closing app, Bye!...\n")
+                break
+        
+        except ConnectionError as err:
+            print('Communication Error: ', err)            
+        
+        finally :     
+            input("\nPress ENTER to continue...")
+
+    
